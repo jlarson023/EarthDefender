aaf498df5234f80e6af7f6238cfc7532431c5f01
jlarson023 <larsontjonathan@gmail.com>
2023-11-28 10:30:04 -0800

    Shooting Started
    Need to make bullet shoot in the direction of the jet.

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * moveSpeed *  Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/BulletSpawn.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawn : MonoBehaviour
+{
+    public GameObject spawnPoint;
+    public GameObject bulletPrefab;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S)) 
+        {
+            Instantiate(bulletPrefab, spawnPoint.transform.position, bulletPrefab.transform.rotation);
+        }
+    }
+}
